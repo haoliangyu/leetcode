3723dc3b9389e0366faeede91475812a5b59ecc1
haoliangyu <njkon@foxmail.com>
2016-11-06 01:51:37 -0500

    finish No.58

---
+++ easy/58.length.of.last.word.js
@@ -0,0 +1,21 @@
+// Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+//
+// If the last word does not exist, return 0.
+//
+// Note: A word is defined as a character sequence consists of non-space characters only.
+//
+// For example,
+// Given s = "Hello World",
+// return 5.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+  s = s.trim();
+
+  for (var i = s.length - 1; i >= 0 && s[i] !== ' '; i--);
+
+  return s.length - i - 1;
+};
