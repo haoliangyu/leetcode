faba74e655f5829d18c74629ba9a3270aef78f49
haoliangyu <njkon@foxmail.com>
2016-11-09 23:16:08 -0500

    finish No.203

---
+++ easy/203.remove.linked.list.elements.js
@@ -0,0 +1,35 @@
+// Remove all elements from a linked list of integers that have value val.
+//
+// Example
+// Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+// Return: 1 --> 2 --> 3 --> 4 --> 5
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  while (head && head.val === val) {
+    head = head.next;
+  }
+
+  var node = head;
+
+  while (node && node.next) {
+    if (node.next.val === val) {
+      node.next = node.next.next;
+    } else {
+      node = node.next;
+    }
+  }
+
+  return head;
+};
