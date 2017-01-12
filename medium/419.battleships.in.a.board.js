12e96304319778d0629a82235e773ad73044dbe6
haoliangyu <njkon@foxmail.com>
2017-01-12 01:10:09 -0500

    initial solution for No.419

---
+++ medium/419.battleships.in.a.board.js
@@ -0,0 +1,77 @@
+// Given an 2D board, count how many different battleships are in it. The battleships are represented with 'X's, empty slots are represented with '.'s. You may assume the following rules:
+//
+//     You receive a valid board, made of only battleships or empty slots.
+//     Battleships can only be placed horizontally or vertically. In other words, they can only be made of the shape 1xN (1 row, N columns) or Nx1 (N rows, 1 column), where N can be of any size.
+//     At least one horizontal or vertical cell separates between two battleships - there are no adjacent battleships.
+//
+// Example:
+//
+// X..X
+// ...X
+// ...X
+//
+// In the above board there are 2 battleships.
+//
+// Invalid Example:
+//
+// ...X
+// XXXX
+// ...X
+//
+// This is an invalid board that you will not receive - as battleships will always have a cell separating between them.
+//
+// Follow up:
+// Could you do it in one-pass, using only O(1) extra memory and without modifying the value of the board?
+
+function counting(x, y, board) {
+  let ship = 0;
+  let preRow = x - 1;
+  let nextRow = x + 1;
+
+  if (board[x][y - 1] === 'X') {
+    ship++;
+  }
+
+  if (board[x][y + 1] === 'X') {
+    ship++;
+  }
+
+  if (board[preRow] !== undefined && board[preRow][y] === 'X') {
+    ship++;
+  }
+
+  if (board[nextRow] !== undefined && board[nextRow][y] === 'X') {
+    ship++;
+  }
+
+  return ship;
+}
+
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+var countBattleships = function(board) {
+
+  let ships = 0;
+
+  if (!board) {
+    return ships;
+  }
+
+  for(let i = 0, n = board.length; i < n; i++) {
+    for(let j = 0, m = board[i].length; j < m; j++) {
+      if (board[i][j] === 'X') {
+        let context = counting(i, j, board);
+
+        if (context === 0) {
+          ships++;
+        } else if (context === 1) {
+          ships += 0.5;
+        }
+      }
+    }
+  }
+
+  return ships;
+};
