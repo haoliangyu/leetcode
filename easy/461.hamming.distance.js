dc10cfde48b8cba7899bdc4c2fd6cfa10ee405cd
haoliangyu <njkon@foxmail.com>
2017-01-13 22:37:50 -0500

    finish No.461

---
+++ easy/461.hamming.distance.js
@@ -0,0 +1,36 @@
+// The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+//
+// Given two integers x and y, calculate the Hamming distance.
+//
+// Note:
+// 0 ≤ x, y < 231.
+//
+// Example:
+//
+// Input: x = 1, y = 4
+//
+// Output: 2
+//
+// Explanation:
+// 1   (0 0 0 1)
+// 4   (0 1 0 0)
+//        ↑   ↑
+//
+// The above arrows point to positions where the corresponding bits are different.
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function(x, y) {
+  let value = x ^ y;
+  let count = 0;
+
+  while(value > 0) {
+    count++;
+    value = value & (value - 1);
+  }
+
+  return count;
+};
