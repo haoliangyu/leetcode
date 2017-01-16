e28f8b9d7143ac8d76869c816bbed5df655a4bad
haoliangyu <njkon@foxmail.com>
2017-01-16 00:43:39 -0500

    better solution for No.463

--- easy/463.island.perimeter.js
+++ easy/463.island.perimeter.js
@@ -11,27 +11,19 @@
 // Explanation: The perimeter is the 16 yellow stripes in the image below:
 //
 
-function cell(x, y, grid, setNull) {
-  if (grid[x] === undefined) {
-    return 0;
-  }
-
-  if (grid[x][y] === null) {
-    return setNull ? 1 : 0;
-  }
-
-  if (grid[x][y] === undefined) {
+function cell(x, y, grid) {
+  if (grid[x] === undefined || grid[x][y] === undefined) {
     return 0;
   }
 
   return grid[x][y];
 }
 
-function neighborhoods(x, y, grid) {
-  return cell(x + 1, y, grid, true) +
-         cell(x - 1, y, grid, true) +
-         cell(x, y + 1, grid, true) +
-         cell(x, y - 1, grid, true);
+function water(x, y, grid) {
+  return cell(x + 1, y, grid) +
+         cell(x - 1, y, grid) +
+         cell(x, y + 1, grid) +
+         cell(x, y - 1, grid);
 }
 
 /**
@@ -39,42 +31,13 @@ function neighborhoods(x, y, grid) {
  * @return {number}
  */
 var islandPerimeter = function(grid) {
-  let places = [];
   let strips = 0;
 
   for (let i = 0, n = grid.length; i < n; i++) {
     for (let j = 0, m = grid[i].length; j < m; j++) {
       if (grid[i][j] === 1) {
-        places.push({ x: i, y: j });
-        grid[i][j] = null;
-        break;
+        strips += water(i, j, grid);
       }
-    }
-  }
-
-  while (places.length > 0) {
-    let current = places.pop();
-
-    strips += 4 - neighborhoods(current.x, current.y, grid);
-
-    if (cell(current.x + 1, current.y, grid) === 1) {
-      places.push({ x: current.x + 1, y: current.y });
-      grid[current.x + 1][current.y] = null;
-    }
-
-    if (cell(current.x - 1, current.y, grid) === 1) {
-      places.push({ x: current.x - 1, y: current.y });
-      grid[current.x - 1][current.y] = null;
-    }
-
-    if (cell(current.x, current.y + 1, grid) === 1) {
-      places.push({ x: current.x, y: current.y + 1 });
-      grid[current.x][current.y + 1] = null;
-    }
-
-    if (cell(current.x, current.y - 1, grid) === 1) {
-      places.push({ x: current.x, y: current.y - 1 });
-      grid[current.x][current.y - 1] = null;
     }
   }
 
