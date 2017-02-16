490f507ff5a165346ade238b694a33e54a10ca4f
haoliangyu <njkon@foxmail.com>
2017-02-16 00:23:42 -0500

    finish No.496

---
+++ easy/496.next.greater.element.i.js
@@ -0,0 +1,48 @@
+// You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+//
+// The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+//
+// Example 1:
+//
+// Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+// Output: [-1,3,-1]
+// Explanation:
+//    For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+//    For number 1 in the first array, the next greater number for it in the second array is 3.
+//    For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+//
+// Example 2:
+//
+// Input: nums1 = [2,4], nums2 = [1,2,3,4].
+// Output: [3,-1]
+// Explanation:
+//    For number 2 in the first array, the next greater number for it in the second array is 3.
+//    For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
+//
+// Note:
+//
+//    All elements in nums1 and nums2 are unique.
+//    The length of both nums1 and nums2 would not exceed 1000.
+
+/**
+ * @param {number[]} findNums
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+let nextGreaterElement = function(findNums, nums) {
+
+  nums.sort((a, b) => a - b);
+
+  let indexes = {};
+  for (let i = 1, n = nums.length; i < n; i++) {
+    indexes[nums[i]] = i;
+  }
+
+  let results = [];
+  for (let i = 1, n = findNums.length; i < n; i++) {
+    let index = indexes[findNums[i]] + 1;
+    results.push(index === nums.length ? -1 : nums[index]);
+  }
+
+  return results;
+};
