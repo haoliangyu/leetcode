fef25be91aee68a09d4080d5760136ac4cc96b8a
haoliangyu <njkon@foxmail.com>
2017-02-19 08:46:41 -0500

    finish No.378

---
+++ medium/378.k.smallest.element.in.a.sorted.matrix.js
@@ -0,0 +1,39 @@
+// Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
+//
+// Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+//
+// Example:
+//
+// matrix = [
+//    [ 1,  5,  9],
+//    [10, 11, 13],
+//    [12, 13, 15]
+// ],
+// k = 8,
+//
+// return 13.
+//
+// Note:
+// You may assume k is always valid, 1 ≤ k ≤ n2.
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+let kthSmallest = function(matrix, k) {
+
+  let n = matrix.length;
+  let possibleNums = [];
+
+  for (let i = 0; i < k && i < n; i++) {
+    let m = Math.floor(k / (i + 1)) - 1;
+    for (let j = 0; j <= m && j < n; j++) {
+      possibleNums.push(matrix[i][j]);
+    }
+  }
+
+  possibleNums.sort((a, b) => a - b);
+
+  return possibleNums[k - 1];
+};
