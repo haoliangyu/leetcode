1e257f66c83a20e8d643d2fcb3b4d4ed18b32880
haoliangyu <njkon@foxmail.com>
2017-03-12 00:57:19 -0500

    a solution for No.40 that beats 1% :frowning_face:

---
+++ medium/40.combination.sum.ii.js
@@ -0,0 +1,52 @@
+// Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+//
+// Each number in C may only be used once in the combination.
+//
+// Note:
+//
+//    All numbers (including target) will be positive integers.
+//    The solution set must not contain duplicate combinations.
+//
+// For example, given candidate set [10, 1, 2, 7, 6, 1, 5] and target 8,
+// A solution set is:
+//
+// [
+//  [1, 7],
+//  [1, 2, 5],
+//  [2, 6],
+//  [1, 1, 6]
+// ]
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+let combinationSum2 = function(candidates, target) {
+  let results = {};
+
+  candidates.sort((a, b) => a - b);
+
+  for (let i = 0, n = candidates.length; i < n; i++) {
+    getCombinations(candidates, target, i, candidates[i], [candidates[i]], results);
+  }
+
+  let values = [];
+  for (let key in results) {
+    values.push(results[key]);
+  }
+
+  return values;
+};
+
+function getCombinations(candidates, target, currentIndex, currentSum, current, results) {
+  if (currentSum === target) {
+    results[current] = current;
+  } else if (currentSum < target) {
+    for (let i = currentIndex + 1, n = candidates.length; i < n; i++) {
+      let next = current.slice();
+      next.push(candidates[i]);
+      getCombinations(candidates, target, i, currentSum + candidates[i], next, results);
+    }
+  }
+}
