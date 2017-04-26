7161606bd8776cb42d69e9ae59bcc3c33791fd08
haoliangyu <njkon@foxmail.com>
2017-04-26 00:08:27 -0400

    finish No.120

--- medium/120.triangle.js
+++ medium/120.triangle.js
@@ -18,6 +18,43 @@
  * @param {number[][]} triangle
  * @return {number}
  */
-var minimumTotal = function(triangle) {
+let minimumTotal = function(triangle) {
 
+  if (triangle.length === 0) {
+    return 0;
+  }
+
+  if (triangle.length === 1) {
+    return triangle[0][0];
+  }
+
+  let sums = new Array(triangle.length);
+  sums[0] = triangle[0][0];
+
+  for (let i = 1, n = triangle.length; i < n; i++) {
+    let previous = Infinity;
+    let j = 0;
+
+    for (; j < i; j++) {
+      let current = sums[j];
+      sums[j] = min(previous + triangle[i][j], current + triangle[i][j]);
+      previous = current;
+    }
+
+    sums[j] = previous + triangle[i][j];
+  }
+
+  let minSum = Infinity;
+
+  for (let sum of sums) {
+    if (sum < minSum) {
+      minSum = sum;
+    }
+  }
+
+  return minSum;
 };
+
+function min(a, b) {
+  return a > b ? b : a;
+}
