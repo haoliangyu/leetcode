ded518e3779918df287989f196217773964fa92e
haoliangyu <njkon@foxmail.com>
2017-04-28 00:18:39 -0400

    finish No.49

---
+++ medium/49.group.anagrams.js
@@ -0,0 +1,33 @@
+// Given an array of strings, group anagrams together.
+//
+// For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
+// Return:
+//
+// [
+//   ["ate", "eat","tea"],
+//   ["nat","tan"],
+//   ["bat"]
+// ]
+//
+// Note: All inputs will be in lower-case.
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+let groupAnagrams = function(strs) {
+
+  let dict = {};
+
+  for (let str of strs) {
+    let key = str.split('').sort().join('');
+
+    if (dict[key]) {
+      dict[key].push(str);
+    } else {
+      dict[key] = [str];
+    }
+  }
+
+  return dict.values();
+};
